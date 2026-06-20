c9988ca64b0f94af2531f4900feb50062a45adf1
Sinem Subaşı <124834205+sinemsubasi@users.noreply.github.com>
2026-06-20 23:34:47 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
-import { serveDir } from "jsr:@std/http/file-server";
+import { serveDir, serveFile } from "jsr:@std/http/file-server";
 
 Deno.serve((req: Request) => {
+  const url = new URL(req.url);
+  
+  // Siteye ilk girildiğinde direkt index.html'i aç
+  if (url.pathname === "/") {
+    return serveFile(req, "./index.html");
+  }
+  
+  // Fotoğraf yüklenmek istendiğinde klasörden bulup getir
   return serveDir(req, {
     fsRoot: ".",
   });
